702364ab0e65a7613ae4f9a7001824d54f1be867
akildevs1000 <akildevs1000@gmail.com>
2026-01-05 18:39:59 +0400

    test

--- backend/app/Http/Controllers/LicenseController.php
+++ backend/app/Http/Controllers/LicenseController.php
@@ -31,7 +31,19 @@ class LicenseController extends Controller
         }
 
         // --------------------
-        // Expiry check added
+        // If expiry_date is NULL -> set 30 days from today (trial) ONCE
+        // --------------------
+        if (is_null($company->expiry_date)) {
+            $company->where("id", $company->id)->update([
+                'expiry_date' => now()->addDays(30)->toDateString(), // YYYY-MM-DD
+            ]);
+
+            // refresh model value for below checks/response formats
+            $company->refresh();
+        }
+
+        // --------------------
+        // Expiry check
         // --------------------
         if ($company->expiry_date) {
             $expiry = Carbon::parse($company->expiry_date)->startOfDay();
